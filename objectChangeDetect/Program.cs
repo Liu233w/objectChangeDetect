20772d2ad0568a2b9b90f03648840ded58ee5722
Liu233w <wwwlsmcom@outlook.com>
2018-04-24 12:23:58 +0800

    done tracking
    may be slow

--- objectChangeDetect/Program.cs
+++ objectChangeDetect/Program.cs
@@ -10,23 +10,18 @@ namespace objectChangeDetect
         {
             var myEntity = new MyEntity();
 
-            using (var c2Tracker = myEntity.TrackedC2())
+            myEntity.C2 = new C2
             {
-                c2Tracker.Obj = new C2
+                C = "c",
+                C1 = new C1
                 {
-                    C = "c",
-                    C1 = new C1
-                    {
-                        A = "a",
-                        B = "b",
-                    },
-                };
-            }
+                    A = "a",
+                    B = "b",
+                },
+            };
 
-            using (var tracker2 = myEntity.TrackedC2())
-            {
-                tracker2.Obj.C = "new C";
-            }
+            myEntity.C2.C = "new C";
+            myEntity.C2.C1.A = "new A";
 
             Console.WriteLine(myEntity.ExtensionData);
         }
@@ -50,47 +45,29 @@ namespace objectChangeDetect
     {
         public string ExtensionData { get; set; }
 
-        public ExtendedObjectTracker<C2> TrackedC2() => this.GetTrackedData<C2>("c2");
+        public C2 C2
+        {
+            get => this.GetTrackedData<C2>("c2");
+            set => this.SetData("c2", value);
+        }
     }
 
     public static class TrackedObjectExtensions
     {
-        public static ExtendedObjectTracker<T> GetTrackedData<T>(
+        public static T GetTrackedData<T>(
             this IExtendableObject extendableObject,
             string name) where T : class
         {
-            return new ExtendedObjectTracker<T>(extendableObject, name);
-        }
-    }
-
-    public class ExtendedObjectTracker<T> : IDisposable
-        where T : class
-    {
-        public T Obj { get; set; }
-
-        private readonly IExtendableObject _entity;
-
-        private readonly string _key;
-
-        public ExtendedObjectTracker(IExtendableObject entity, string key)
-        {
-            _entity = entity;
-            _key = key;
-
-            Obj = entity.GetData<T>(key).AsTrackable();
-        }
-
-        public void CheckAndUpdate()
-        {
-            if (Obj.CastToIChangeTrackable<T>().IsChanged)
+            var obj = extendableObject.GetData<T>(name).AsTrackable();
+            obj.CastToIChangeTrackable()
+                .PropertyChanged += (sender, args) =>
             {
-                _entity.SetData(_key, Obj);
-            }
-        }
+                var trackable = (IChangeTrackable<T>)sender;
+                trackable.AcceptChanges();
 
-        public void Dispose()
-        {
-            CheckAndUpdate();
+                extendableObject.SetData(name, trackable.GetOriginal());
+            };
+            return obj;
         }
     }
 }
